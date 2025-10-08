c8a244d251e9822f019a218e68244433c4474c71
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-08 11:25:05 +0200

    refactor: translate names to english

--- Ejercicio-1/src/Main.java
+++ Ejercicio-1/src/Main.java
@@ -3,52 +3,52 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-//instanciamos cada objeto mes
-        Month enero= new Month("enero");
-        Month febrero= new Month("febrero");
-        Month marzo= new Month("marzo");
-        Month abril= new Month("abril");
-        Month mayo= new Month("mayo");
-        Month junio= new Month("junio");
-        Month julio= new Month("julio");
-        Month setiembre= new Month("setiembre");
-        Month agosto= new Month("agosto");
-        Month octubre= new Month("octubre");
-        Month noviembre= new Month("noviembre");
-        Month diciembre= new Month("diciembre");
-//creamos array de meses
 
-        ArrayList <Month> meses = new ArrayList<>();
+        Month january= new Month("January");
+        Month February= new Month("February");
+        Month March= new Month("March");
+        Month April= new Month("April");
+        Month May= new Month("May");
+        Month June= new Month("June");
+        Month July= new Month("July");
+        Month September= new Month("September");
+        Month August= new Month("August");
+        Month October= new Month("October");
+        Month November= new Month("November");
+        Month December= new Month("December");
 
-//agregamos cada mes al array
-        meses.add(enero);
-        meses.add(febrero);
-        meses.add(marzo);
-        meses.add(abril);
-        meses.add(mayo);
-        meses.add(junio);
-        meses.add(julio);
-        meses.add(setiembre);
-        meses.add(octubre);
-        meses.add(noviembre);
-        meses.add(diciembre);
 
-        //imprimimos el listado sin agosto
-        System.out.println("--Año sin Agosto--");
+        ArrayList <Month> months = new ArrayList<>();
+
+
+        months.add(january);
+        months.add(February);
+        months.add(March);
+        months.add(April);
+        months.add(May);
+        months.add(June);
+        months.add(July);
+        months.add(September);
+        months.add(October);
+        months.add(November);
+        months.add(December);
+
+
+        System.out.println("--List without August--");
         System.out.println();
 
-        for (Month m: meses){
+        for (Month m: months){
             System.out.println(m.name);
         }
         System.out.println();
 
-//agregamos agosto al listado en la posición correcta
-        meses.add(7, agosto);
 
-        System.out.println("--Año con agosto--");
+        months.add(7, August);
+
+        System.out.println("--Year with August--");
         System.out.println();
-//volvemos a imprimir el listado con el mes de agosto incluido
-        for (Month m: meses){
+
+        for (Month m: months){
             System.out.println(m.name);
         }
 

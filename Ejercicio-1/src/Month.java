93c443ae6b2f27ded279e4782f83d7b9141e4301
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-08 11:17:47 +0200

    feature: adding guard clauses for field validation

--- Ejercicio-1/src/Month.java
+++ Ejercicio-1/src/Month.java
@@ -3,6 +3,13 @@ public class Month {
     String name;
 
     Month(String name){
+        if (name==(null)){
+            throw new IllegalArgumentException("The name of the product can't be null");
+        }
+
+        if (name.isBlank()){
+            throw new IllegalArgumentException("The name of the month can't be empty");
+        }
         this.name=name;
     }
 
